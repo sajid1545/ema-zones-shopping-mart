e23fc0dbbb38eb9a956c8b2b9697a8d573242d9d
sajid9962 <clashking1545@gmail.com>
2022-10-18 08:42:30 +0600

    inventory updated

--- src/Components/Inventory/Inventory.jsx
+++ src/Components/Inventory/Inventory.jsx
@@ -2,9 +2,68 @@ import React from 'react';
 
 const Inventory = () => {
 	return (
-		<div className='text-center'>
-			<h1>This is Inventory</h1>
-			<h1>Nwe Products Coming soon</h1>
+		<div>
+			<div className="text-center p-4 text-2xl font-semibold">
+				<h1>This is Inventory</h1>
+				<h1>New Products Coming soon</h1>
+			</div>
+
+			<div>
+				<section className="py-6 bg-gray-800 text-gray-50">
+					<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+						<img
+							src="https://img.freepik.com/free-psd/black-polo-shirt-mockup_125540-1459.jpg?w=996&t=st=1666060621~exp=1666061221~hmac=b45c2d69876f2ed0a91f615d982d252b8d15e205e62ffca2bb0248fab50b5733"
+							alt=""
+							className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 bg-gray-500 aspect-square"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-psd/baby-oil-mockup_187299-14999.jpg?w=996&t=st=1666060580~exp=1666061180~hmac=cfe4ec33bf92939c99f23f3248429043c268e2d0c4934af20e7493b185ef6251"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-photo/headphones-african-american-woman-s-portrait-isolated-dark-studio-background-multicolored-neon-light-beautiful-female-model-concept-human-emotions-facial-expression-sales-ad-fashion_155003-34470.jpg?w=996&t=st=1666060649~exp=1666061249~hmac=e7916f184213427a3284515c1ac43c8670b8e5e66acd209c4aec156478ea94f8"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-photo/red-gold-headphones-digital-device_53876-97303.jpg?w=996&t=st=1666060666~exp=1666061266~hmac=89f2e021cf9395ea19d2412d0e5e5435bbb5abe66ce047459c56f0492ad2e45f"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-photo/shoes_1203-8153.jpg?w=996&t=st=1666060698~exp=1666061298~hmac=3884cf45ba4f6626685ba775e7f3cda0c096655c08af5ba26eeb8b8b1d393c7a"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-photo/brown-man-s-leather-derby-shoes_53876-97144.jpg?w=996&t=st=1666060715~exp=1666061315~hmac=bc43bae2fa091d20481698dfd386f477d3f9f3fa7b4dec1fad9eac24ce4c33bd"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-psd/metal-flask-bottle-mockup_439185-1341.jpg?w=900&t=st=1666060739~exp=1666061339~hmac=7c1848166769d8eb2d09ec343d19ae5f0aaa2d3d7a79d4cf2a17df96e4ca1765"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://img.freepik.com/free-psd/man-wearing-jogger-pants-hoodie-mockup_23-2149211978.jpg?w=360&t=st=1666060795~exp=1666061395~hmac=61826dc0dc5b1c419eb9db14065273a88a684e15aba0c33043c957cccec82532"
+						/>
+						<img
+							alt=""
+							className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square"
+							src="https://images.unsplash.com/photo-1473966968600-fa801b869a1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+						/>
+						<img
+							src="https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1201&q=80"
+							alt=""
+							className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 bg-gray-500 aspect-square"
+						/>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 };
